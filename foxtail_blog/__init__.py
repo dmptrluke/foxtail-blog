f5abdd0db8961bf9fc1ff6062f6f677df3293da9
Luke <lukeroge@gmail.com>
2019-11-19 00:30:16 +1300

    Add a pretty name.

--- foxtail_blog/__init__.py
+++ foxtail_blog/__init__.py
@@ -1,3 +1,3 @@
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 2)
 
 default_app_config = "foxtail_blog.apps.BlogConfig"
--- foxtail_blog/apps.py
+++ foxtail_blog/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = 'foxtail_blog'
+    verbose_name = 'Foxtail Blog'
 
     def ready(self):
         # import signal handlers
