a8149880c1ea14ddd020fda1eff09ea19c86a175
Luke Rogers <luke@dmptr.com>
2019-12-02 17:23:17 +1300

    Remove image field warmer.

--- foxtail_blog/apps.py
+++ foxtail_blog/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class BlogConfig(AppConfig):
     name = 'foxtail_blog'
     verbose_name = 'Foxtail Blog'
-
-    def ready(self):
-        # import signal handlers
-        # noinspection PyUnresolvedReferences
-        import foxtail_blog.signals
